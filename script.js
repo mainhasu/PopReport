4ab72fbd5793e11c46b74fc0dc81fc3dace74e6c
Rik <mainhasu9@gmail.com>
2026-08-20 23:11:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const corOriginal=getComputedStyle(document.body).backgroundColor;
+
+function alterarFundo(){
+  let cor = prompt("Digite uma cor (de preferência em hexadecimal):");
+
+  if (cor) {
+  document.body.style.backgroundColor= cor;
+  }
+}
+function voltarFundo(){
+  document.body.style.backgroundColor= corOriginal;
+}
+
+const list = []
+const formulario = document.getElementById('form');
+formulario.addEventListener('submit', function(event){
+  event.preventDefault();
+  const novoNome = document.getElementById('nome').value
+  const novaSenha = document.getElementById('senha').value
+
+  const novo = {
+    nome: novoNome,
+    senha: novaSenha,
+  };
+  list.push(novo);
+  console.log(list);
+
+  document.getElementById('nome').value = "";
+  document.getElementById('senha').value = "";
+});
+    
